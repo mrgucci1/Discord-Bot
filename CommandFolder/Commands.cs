57d1193c22217797c63ba60a5a2ab95a18e681dc
mrgucci1 <56367372+mrgucci1@users.noreply.github.com>
2020-09-08 00:58:24 -0500

    Add files via upload

---
+++ CommandFolder/Commands.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Discord_Bot___Bob.CommandFolder
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("spam")]
+        public async Task Spam([Remainder]string text)
+        {
+           for(int i = 0;i< 10;i++)
+            {
+                await ReplyAsync(text);
+                await PutTaskDelay();
+            }
+        }
+
+        [Command("mostbeautifulgirl")]
+        public async Task princess()
+        {
+            await ReplyAsync("jessie");
+        }
+
+        [Command("game")]
+        public async Task chooseGame([Remainder]string text)
+        {
+            var rand = new Random();
+            string[] games = text.Split(',');
+            await ReplyAsync($"The Game Options are: ");
+            for (int i = 0; i < games.Length; i++)
+            {
+                await ReplyAsync($"{games[i]}");
+                await PutTaskDelay();
+            }
+            await PutTaskDelay();
+            await ReplyAsync($"\nChoosing........");
+            await PutTaskDelay();
+            await ReplyAsync($"\n.......................");
+            await PutTaskDelay();
+            await ReplyAsync($"\n..................................");
+            int choice = rand.Next(0, games.Length);
+            await ReplyAsync($"\nGAME IS: {games[choice]}");
+        }
+
+        [Command("shee")]
+        public async Task sheeee()
+        {
+            await ReplyAsync("!sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        }
+
+        [Command("play shee")]
+        public async Task sheeee2()
+        {
+            await ReplyAsync("!play molly remix playboi");
+        }
+ 
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(1000);
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discord_Bot___Bob
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient disClient;
+        private CommandService disCommands;
+        private IServiceProvider disServices;
+
+        public async Task RunBotAsync()
+        {
+            disClient = new DiscordSocketClient();
+            disCommands = new CommandService();
+            disServices = new ServiceCollection()
+                .AddSingleton(disClient)
+                .AddSingleton(disCommands)
+                .BuildServiceProvider();
+
+            string botToken = "";
+
+            disClient.Log += DisClient_Log;
+
+            await RegisterCommandsAsync();
+
+            await disClient.LoginAsync(TokenType.Bot, botToken);
+
+            await disClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task DisClient_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            disClient.MessageReceived += HandleCommandAsync;
+            await disCommands.AddModulesAsync(Assembly.GetEntryAssembly(), disServices);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(disClient, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix(".", ref argPos))
+            {
+                var result = await disCommands.ExecuteAsync(context, argPos, disServices);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+
+            }
+
+        }
+    }
+
+    
+
+}
